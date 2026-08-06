cc6580abfbaee4b772597215dc654e20f821bcca
Roman <vasylukroma77@gmail.com>
2026-08-06 13:17:18 +0200

    style: add and import reset.css for cross-browser normalization

--- CREATEX/src/App.jsx
+++ CREATEX/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import "./reset.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
