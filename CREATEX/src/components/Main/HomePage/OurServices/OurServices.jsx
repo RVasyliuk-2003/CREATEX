90ebb735d2465c9e956b698dbf965ab569c81e91
Roman <vasylukroma77@gmail.com>
2026-08-08 17:11:25 +0200

    feat: complete OurService section layout and styles

--- CREATEX/src/components/Main/HomePage/OurServices/OurServices.jsx
+++ CREATEX/src/components/Main/HomePage/OurServices/OurServices.jsx
@@ -1,9 +1,44 @@
-import styles from "./ourServices.module.css";
+import style from "./ourServices.module.css";
+
+import construction from "./images/ic-construction.svg";
+import painting from "./images/ic-painting.svg";
+import pantone from "./images/ic-pantone.svg";
+import plan from "./images/ic-plan.svg";
 
 const OurServices = () => {
   return (
-    <section>
-      <div className="container"></div>
+    <section className={`containerForImgBacground ${style.imgUrl}`}>
+      <div className="container">
+        <div className={style.mainBoxServices}>
+          <h2>Our services</h2>
+          <p>
+            Createx Construction Bureau is a construction giant with a full
+            range of construction services.
+          </p>
+          <div className={style.serviContainer}>
+            <div className={style.serviBox}>
+              <img src={construction} alt="construction" />
+              <p>Construction</p>
+            </div>
+            <div className={style.serviBox}>
+              <img src={plan} alt="plan" />
+              <p>Project Development</p>
+            </div>
+            <div className={style.serviBox}>
+              <img src={construction} alt="pantone" />
+              <p>Interior Design</p>
+            </div>
+            <div className={style.serviBox}>
+              <img src={painting} alt="painting" />
+              <p>Repairs</p>
+            </div>
+          </div>
+          <div className={style.btnServiBox}>
+            <p>Learn more about our services</p>
+            <button>VIEW SERVICES</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
