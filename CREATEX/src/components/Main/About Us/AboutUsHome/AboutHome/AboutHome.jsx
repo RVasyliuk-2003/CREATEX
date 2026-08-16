4696a7a736937b214b22e0de4b3ac2b7b888bbaa
Roman <vasylukroma77@gmail.com>
2026-08-16 20:14:20 +0200

    feat(about): implement main hero section with responsive image layout

--- CREATEX/src/components/Main/About Us/AboutUsHome/AboutHome/AboutHome.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/AboutHome/AboutHome.jsx
@@ -1,9 +1,35 @@
-import styles from "./aboutHome.module.css";
+import style from "./aboutHome.module.css";
+import img1 from "./images/img1.png";
+import img2 from "./images/img2.png";
+
+import { Link } from "react-router-dom";
 
 const AboutHome = () => {
   return (
-    <section>
-      <div className="container"></div>
+    <section
+      className={style.maxSizeSection}
+      style={{ backgroundColor: "#F4F5F6" }}
+    >
+      <div className="container">
+        <div className={style.relativeBox}>
+          <div className={style.HeroContainer}>
+            <span>
+              <Link to="/AboutHome">Homepage</Link> /
+              <Link to="/AboutUs">About Us</Link>
+            </span>
+
+            <h1>ABOUT US</h1>
+
+            <p>
+              Createx Construction Bureau has been successfully operating in the
+              USA construction market since 2000. We are proud to offer you
+              quality construction and exemplary service.
+            </p>
+          </div>
+        </div>
+      </div>
+      <img className={style.img1} src={img1} alt="img1" />
+      <img className={style.img2} src={img2} alt="img2" />
     </section>
   );
 };
