fc687164ce80b3365c316d551b8e2c36bf7d2e15
Roman <vasylukroma77@gmail.com>
2026-08-20 13:16:50 +0200

    feat(about): add mapWork component with CEO quote and styling

--- CREATEX/src/components/Main/About Us/AboutUsHome/MapWork/MapWork.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/MapWork/MapWork.jsx
@@ -1,16 +1,29 @@
-import styles from "./mapWork.module.css";
+import style from "./mapWork.module.css";
 
-import map from "./images/map.svg";
 import australia from "./images/australia.png";
 import canada from "./images/canada.png";
 import china from "./images/china.png";
 import ukraine from "./images/ukraine.png";
 import usa from "./images/usa.png";
+import germany from "./images/germany.png";
 
 const MapWork = () => {
   return (
-    <section>
-      <div className="container"></div>
+    <section className={style.backgroundColorSection}>
+      <div className="container">
+        <div className={style.mainContainerWorld}>
+          <h2>We work worldwide</h2>
+
+          <div className={style.imgMapbackground}>
+            <img className={style.australia} src={australia} alt="australia" />
+            <img className={style.canada} src={canada} alt="canada" />
+            <img className={style.china} src={china} alt="china" />
+            <img className={style.ukraine} src={ukraine} alt="ukraine" />
+            <img className={style.usa} src={usa} alt="usa" />
+            <img className={style.germany} src={germany} alt="germany" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
