c291f2a1bbe8e4da471a0c16d433769541984bd5
Roman <vasylukroma77@gmail.com>
2026-08-20 10:57:32 +0200

    feat(about): add images partners for OurPartners file

--- CREATEX/src/components/Main/About Us/AboutUsHome/OurPartners/OurPartners.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/OurPartners/OurPartners.jsx
@@ -1,5 +1,16 @@
 import styles from "./ourPartners.module.css";
 
+import part1 from "./images/part1.png";
+import part2 from "./images/part2.png";
+import part3 from "./images/part3.png";
+import part4 from "./images/part4.png";
+import part5 from "./images/part5.png";
+import part6 from "./images/part6.png";
+import part7 from "./images/part7.png";
+import part8 from "./images/part8.png";
+import part9 from "./images/part9.png";
+import part10 from "./images/part10.png";
+
 const OurPartners = () => {
   return (
     <section>
