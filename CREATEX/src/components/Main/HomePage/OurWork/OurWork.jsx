53ec7e90869cc0db0fe2cbb0695b34f56891bf78
Roman <vasylukroma77@gmail.com>
2026-08-20 17:45:02 +0200

    feat(our-work): add Swiper slider for project cards

--- CREATEX/src/components/Main/HomePage/OurWork/OurWork.jsx
+++ CREATEX/src/components/Main/HomePage/OurWork/OurWork.jsx
@@ -7,7 +7,14 @@ import Building1 from "./images/image.png";
 import Building2 from "./images/image2.png";
 import Building3 from "./images/image3.jpg";
 
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+
 const OurWork = () => {
+  const swiperRef = useRef(null);
+
   return (
     <section>
       <div className="container">
@@ -17,39 +24,70 @@ const OurWork = () => {
               Browse our selected projects <br /> and learn more about our work
             </h2>
             <div className={style.mainTextBtnSliderBox}>
-              <button>
+              <button onClick={() => swiperRef.current?.slidePrev()}>
                 <img src={Left} alt="Left" />
               </button>
-              <button>
+              <button onClick={() => swiperRef.current?.slideNext()}>
                 <img src={Right} alt="Right" />
               </button>
             </div>
           </div>
 
-          <div className={style.mainCartSliderBox}>
-            <div className={style.cartSlider}>
-              <img src={Building1} alt="Building1" />
-              <div>
-                <h5>Red Finger Building</h5>
-                <p>Business Centers</p>
+          <Swiper
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
+            modules={[Navigation]}
+            spaceBetween={20}
+            slidesPerView={3}
+            className={style.mainCartSliderBox}
+          >
+            <SwiperSlide>
+              <div className={style.cartSlider}>
+                <img src={Building1} alt="Building1" />
+                <div>
+                  <h5>Red Finger Building</h5>
+                  <p>Business Centers</p>
+                </div>
               </div>
-            </div>
-            <div className={style.cartSlider}>
-              <img src={Building2} alt="Building2" />
-              <div>
-                <h5>Cubes Building</h5>
-                <p>Business Centers</p>
-              </div>
-            </div>
-            <div className={style.cartSlider}>
-              <img src={Building3} alt="Building3" />
-              <div>
-                <h5>The Pencil Building</h5>
-                <p>Stores & Malls</p>
-              </div>
-            </div>
-          </div>
+            </SwiperSlide>
 
+            <SwiperSlide>
+              <div className={style.cartSlider}>
+                <img src={Building2} alt="Building2" />
+                <div>
+                  <h5>Cubes Building</h5>
+                  <p>Business Centers</p>
+                </div>
+              </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <div className={style.cartSlider}>
+                <img src={Building3} alt="Building3" />
+                <div>
+                  <h5>The Pencil Building</h5>
+                  <p>Stores & Malls</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={style.cartSlider}>
+                <img src={Building3} alt="Building3" />
+                <div>
+                  <h5>The Pencil Building</h5>
+                  <p>Stores & Malls</p>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={style.cartSlider}>
+                <img src={Building3} alt="Building3" />
+                <div>
+                  <h5>The Pencil Building</h5>
+                  <p>Stores & Malls</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
           <div className={style.btnPortfolio}>
             <p>Explore all our works</p>
             <button>VIEW PORTFOLIO</button>
