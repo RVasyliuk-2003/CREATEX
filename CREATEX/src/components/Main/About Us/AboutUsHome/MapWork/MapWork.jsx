034d4acef9811681d7a9f841403de76d48221e4e
Roman <vasylukroma77@gmail.com>
2026-08-20 12:41:33 +0200

    feat(about): add images countries teams for OurTeam file

--- CREATEX/src/components/Main/About Us/AboutUsHome/MapWork/MapWork.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/MapWork/MapWork.jsx
@@ -1,5 +1,12 @@
 import styles from "./mapWork.module.css";
 
+import map from "./images/map.svg";
+import australia from "./images/australia.png";
+import canada from "./images/canada.png";
+import china from "./images/china.png";
+import ukraine from "./images/ukraine.png";
+import usa from "./images/usa.png";
+
 const MapWork = () => {
   return (
     <section>
