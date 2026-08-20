6600506cf45f8d0852a03b36641cacabf6e20e88
Roman <vasylukroma77@gmail.com>
2026-08-20 12:24:09 +0200

    feat(about): add OurTeam component with CEO quote and styling

--- CREATEX/src/components/Main/About Us/AboutUsHome/OurTeam/OurTeam.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/OurTeam/OurTeam.jsx
@@ -1,4 +1,4 @@
-import styles from "./ourTeam.module.css";
+import style from "./ourTeam.module.css";
 
 import team1 from "./images/team1.jpg";
 import team2 from "./images/team2.jpg";
@@ -12,7 +12,79 @@ import team8 from "./images/team8.jpg";
 const OurTeam = () => {
   return (
     <section>
-      <div className="container"></div>
+      <div className="container">
+        <div className={style.mainContainer}>
+          <h1>Our team</h1>
+          <p>People are at the heart of Createx Construction Bureau </p>
+
+          <div className={style.teamsMainBox}>
+            <div className={style.teamCard}>
+              <img src={team8} alt="team8" />
+              <div className={style.teamCardText}>
+                <h5>Courtney Alexander</h5>
+                <span>CEO, Co-Founder</span>
+              </div>
+            </div>
+
+            <div className={style.teamCard}>
+              <img src={team7} alt="team7" />
+              <div className={style.teamCardText}>
+                <h5>Calvin Fox</h5>
+                <span>CTO, Co-Founder</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team6} alt="team6" />
+              <div className={style.teamCardText}>
+                <h5>Johnny Lane</h5>
+                <span>Commercial Manager</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team5} alt="team5" />
+              <div className={style.teamCardText}>
+                <h5>Diane Mccoy</h5>
+                <span>Director of Human Resources</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team4} alt="team4" />
+              <div className={style.teamCardText}>
+                <h5>Judith Warren</h5>
+                <span>Lead Accountant</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team3} alt="team3" />
+              <div className={style.teamCardText}>
+                <h5>Floyd Simmmons</h5>
+                <span>Finacial Director</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team2} alt="team2" />
+              <div className={style.teamCardText}>
+                <h5>Serenity Edwards</h5>
+                <span>Director of Marketing</span>
+              </div>
+            </div>
+            <div className={style.teamCard}>
+              <img src={team1} alt="team1" />
+              <div className={style.teamCardText}>
+                <h5>Shawn Edwards</h5>
+                <span>Tech Lead</span>
+              </div>
+            </div>
+          </div>
+
+          <div className={style.availableLinkBox}>
+            <p>
+              Become a part of the best team in the construction market of the
+              USA. <a href="#">Available Positions</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
