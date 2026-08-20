dc59e7b121f0bec14b11c1443dc36d669c560e50
Roman <vasylukroma77@gmail.com>
2026-08-20 11:33:28 +0200

    feat(about): add images teams for OurTeam file

--- CREATEX/src/components/Main/About Us/AboutUsHome/OurTeam/OurTeam.jsx
+++ CREATEX/src/components/Main/About Us/AboutUsHome/OurTeam/OurTeam.jsx
@@ -1,5 +1,14 @@
 import styles from "./ourTeam.module.css";
 
+import team1 from "./images/team1.jpg";
+import team2 from "./images/team2.jpg";
+import team3 from "./images/team3.jpg";
+import team4 from "./images/team4.jpg";
+import team5 from "./images/team5.jpg";
+import team6 from "./images/team6.jpg";
+import team7 from "./images/team7.jpg";
+import team8 from "./images/team8.jpg";
+
 const OurTeam = () => {
   return (
     <section>
