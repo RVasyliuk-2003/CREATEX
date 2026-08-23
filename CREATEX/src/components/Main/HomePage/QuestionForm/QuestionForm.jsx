b1f4f4acc7523814bbd6e66e443b097c09f75272
Roman <vasylukroma77@gmail.com>
2026-08-23 11:09:21 +0200

    feat(QuestionForm): add form validation with error messages

--- CREATEX/src/components/Main/HomePage/QuestionForm/QuestionForm.jsx
+++ CREATEX/src/components/Main/HomePage/QuestionForm/QuestionForm.jsx
@@ -1,6 +1,35 @@
 import style from "./questionForm.module.css";
+import { useState } from "react";
 
 const QuestionForm = () => {
+  const nameRegex = /^[A-Za-zА-Яа-яЄєІіЇїҐґ\s'-]{2,50}$/;
+  const phoneRegex =
+    /^(?:\+?38)?[\s\(-]*0\d{2}[\s\)-]*\d{3}[\s-]*\d{2}[\s-]*\d{2}$/;
+
+  const [error, setError] = useState("");
+  const [nameInpt, setNameInpt] = useState("");
+  const [phoneInpt, setPhoneInpt] = useState("");
+  const [commentInpt, setCommentInpt] = useState("");
+
+  const resultForm = () => {
+    if (!nameInpt || !phoneInpt || !commentInpt) {
+      setError("INVALID");
+      return;
+    }
+    if (!nameRegex.test(nameInpt)) {
+      setError("INVALID NAME");
+      return;
+    }
+    if (!phoneRegex.test(phoneInpt)) {
+      setError("INVALID PHONE");
+      return;
+    } else {
+      setError("DONE");
+      setNameInpt("");
+      setPhoneInpt("");
+      setCommentInpt("");
+    }
+  };
   return (
     <section>
       <div className="container">
@@ -9,23 +38,39 @@ const QuestionForm = () => {
           <div className={style.inpBox}>
             <div className={style.inpt}>
               <p>Name</p>
-              <input placeholder="Your name" type="text" />
+              <input
+                value={nameInpt}
+                onChange={(e) => setNameInpt(e.target.value)}
+                placeholder="Your name"
+                type="text"
+              />
             </div>
             <div className={style.inpt}>
               <p>Phone</p>
-              <input placeholder="Your phone" type="number" />
+              <input
+                value={phoneInpt}
+                onChange={(e) => setPhoneInpt(e.target.value)}
+                placeholder="Your phone"
+                type="number"
+              />
             </div>
             <div className={style.inpt}>
               <p>Message</p>
               <input
+                value={commentInpt}
+                onChange={(e) => setCommentInpt(e.target.value)}
                 id={style.textInpt3}
                 // className={style.textInpt3}
                 placeholder="Your message"
                 type="text"
               />
             </div>
-            <button>SEND</button>
+            <button onClick={() => resultForm()}>SEND</button>
           </div>
+
+          {error !== "" ? (
+            <p style={{ color: error === "DONE" ? "green" : "red" }}>{error}</p>
+          ) : null}
         </div>
       </div>
     </section>
