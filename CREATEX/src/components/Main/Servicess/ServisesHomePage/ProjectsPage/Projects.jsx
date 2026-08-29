9dcc7b6fc2ceadadf841a898463b809bcfd8c573
Roman <vasylukroma77@gmail.com>
2026-08-29 23:27:38 +0200

    chore(Services/Projects): add images page and setup component imports

--- CREATEX/src/components/Main/Servicess/ServisesHomePage/ProjectsPage/Projects.jsx
+++ CREATEX/src/components/Main/Servicess/ServisesHomePage/ProjectsPage/Projects.jsx
@@ -1,4 +1,10 @@
-import styles from "./projects.module.css";
+import style from "./projects.module.css";
+
+import fone from "./images/fone.png";
+import img1 from "./images/img1.png";
+import img2 from "./images/img2.png";
+import img3 from "./images/img3.png";
+import img4 from "./images/img4.png";
 
 const Projects = () => {
   return (
